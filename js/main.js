485102b7a2425b5889afcd4790d9939dfb8a2eee
Alao Ifeoluwa <78512561+thealaoifeoluwa@users.noreply.github.com>
2021-10-10 17:54:49 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+HTML CSS JSResult Skip Results Iframe
+document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+
+Resources1× 0.5× 0.25×Rerun
